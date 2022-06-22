9492404c861fe24ac936bf9973ca869c9b768572
devcjlozano <carlosjlp@gmail.com>
2022-06-22 16:46:11 +0200

    DOM- Introducción

---
+++ js/dom.js
@@ -0,0 +1,26 @@
+/* console.log(window);
+console.log(document);
+
+const texto = "Hola, soy Carlos Javier Lozano da Madrid";
+const hablar = (texto) =>
+  speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+
+hablar(texto); */
+
+console.log("************ Elementos del documento **************");
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.doctype);
+console.log(document.characterSet);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
+setTimeout(() => {
+  console.log(document.getSelection().toString());
+}, 3000);
+document.write("<h2> Hola mundo desde el DOM </h2>");
