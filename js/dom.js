91d9178c1efcf214f4ba9ae8273b8da3a197070a
devcjlozano <carlosjlp@gmail.com>
2022-07-05 17:23:16 +0200

    DOM estilos y variables CSS

--- js/dom.js
+++ js/dom.js
@@ -40,7 +40,7 @@ console.log(document.querySelector(".card"));
 console.log(document.querySelectorAll(".card")[2]);
 console.log(document.querySelectorAll("#menu li")); */
 
-console.log(document.documentElement.lang);
+/* console.log(document.documentElement.lang);
 console.log(document.documentElement.getAttribute("lang"));
 console.log(document.querySelector(".link-dom").href);
 console.log(document.querySelector(".link-dom").getAttribute("href"));
@@ -57,11 +57,11 @@ $linkDOM.setAttribute("rel", "noopener");
 $linkDOM.setAttribute("href", "https://youtube.com/jonmircha");
 console.log($linkDOM.hasAttribute("rel"));
 $linkDOM.removeAttribute("rel");
-console.log($linkDOM.hasAttribute("rel"));
+console.log($linkDOM.hasAttribute("rel")); */
 
 // Data-Atributtes
 
-console.log($linkDOM.getAttribute("data-description"));
+/* console.log($linkDOM.getAttribute("data-description"));
 console.log($linkDOM.dataset);
 console.log($linkDOM.dataset.description);
 $linkDOM.setAttribute("data-description", "Modelo de Objeto del documento");
@@ -70,4 +70,43 @@ $linkDOM.dataset.description = "Suscríbete al canal de Jon Mirha y comparte";
 console.log($linkDOM.dataset.description);
 console.log($linkDOM.hasAttribute("data-id"));
 $linkDOM.removeAttribute("data-id");
-console.log($linkDOM.hasAttribute("data-id"));
+console.log($linkDOM.hasAttribute("data-id")); */
+
+// DOM: ESTILOS y VARIABLES CSS
+
+const $linkDOM = document.querySelector(".link-dom");
+
+console.log($linkDOM.style);
+console.log($linkDOM.style.backgroundColor);
+console.log($linkDOM.style.color);
+console.log(window.getComputedStyle($linkDOM));
+console.log(window.getComputedStyle($linkDOM).getPropertyValue("color"));
+
+$linkDOM.style.setProperty("text-decoration", "none");
+$linkDOM.style.setProperty("display", "block");
+$linkDOM.style.width = "50%";
+$linkDOM.style.textAlign = "center";
+$linkDOM.style.marginLeft = "auto";
+$linkDOM.style.marginRight = "auto";
+$linkDOM.style.padding = "1rem";
+$linkDOM.style.borderRadius = ".5rem";
+
+console.log($linkDOM.style);
+
+// Variables CSS - Custom Properties CSS
+
+const $html = document.documentElement;
+$body = document.body;
+
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+let varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color");
+
+console.log(varDarkColor, varYellowColor);
+
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor;
+
+$html.style.setProperty("--dark-color", "#000");
+varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+
+$body.style.setProperty("background-color", varDarkColor);
