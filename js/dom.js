33f68a783752f3af60deac38636b2736c50faf66
devcjlozano <carlosjlp@gmail.com>
2022-07-07 16:46:04 +0200

    DOM Traversing: Recorriendo el DOM

--- js/dom.js
+++ js/dom.js
@@ -135,7 +135,7 @@ $card.classList.toggle("opacity-80", "sepia"); */
 
 // DOM: Texto y html
 
-const $whatIsDOM = document.getElementById("que-es");
+/* const $whatIsDOM = document.getElementById("que-es");
 
 let text = `
   <p> 
@@ -154,4 +154,20 @@ let text = `
 // $whatIsDOM.innerText = text; // Creada para internet explorer, no es la standard
 $whatIsDOM.textContent = text;
 $whatIsDOM.innerHTML = text;
-$whatIsDOM.outerHTML = text;
+$whatIsDOM.outerHTML = text; */
+
+// DOM Traversing: Recorriendo el DOM
+
+const $cards = document.querySelector(".cards");
+
+console.log($cards);
+console.log($cards.children);
+console.log($cards.children[2]);
+console.log($cards.parentElement);
+console.log($cards.firstElementChild);
+console.log($cards.lastElementChild);
+console.log($cards.previousElementSibling);
+console.log($cards.nextElementSibling);
+console.log($cards.closest("div"));
+console.log($cards.closest("body"));
+console.log($cards.children[3].closest("section"));
